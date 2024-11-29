257aedeec0d16adb9539632393148da17a2689f2
zacharyzhang1208 <zacharyzhang1208@hotmail.com>
2024-11-29 16:32:42 +0800

    move wallet

--- front-end/src/constants/userTypes.js
+++ front-end/src/constants/userTypes.js
@@ -5,12 +5,12 @@ export const USER_TYPES = {
 
 // 定义每种用户类型的路由配置
 export const USER_ROUTES = {
-    [USER_TYPES.STUDENT]: [
-        { path: '/wallet', icon: 'fas fa-user', label: 'Wallet' },
+    [USER_TYPES.STUDENT]: [   
         { path: '/student_courses', icon: 'fas fa-book', label: 'My Courses' },
         { path: '/student_register_courses', icon: 'fas fa-book', label: 'Register Course' },
     ],
     [USER_TYPES.TEACHER]: [
+        { path: '/wallet', icon: 'fas fa-user', label: 'Wallet' },
         { path: '/teacher_courses', icon: 'fas fa-chalkboard-teacher', label: 'Teaching Courses' },
     ]
 };
